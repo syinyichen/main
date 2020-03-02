ac7a18cba1b592c1bef32668fe05d93e00a4bee0
CheyanneSim <Cheyanne@u.nus.edu>
2020-03-02 18:02:54 +0800

    Remove unused method addDebt

--- src/main/java/seedu/address/model/transaction/Debt.java
+++ src/main/java/seedu/address/model/transaction/Debt.java
@@ -1,8 +1,6 @@
 package seedu.address.model.transaction;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import seedu.address.model.tag.Tag;
 
@@ -17,38 +15,6 @@ public class Debt extends Transaction {
      */
     public Debt(Description description, Amount amount, LocalDate date) {
         super(description, amount, date, new Tag("Debt"));
-    }
-
-    public Description getDescription() {
-        return description;
-    }
-
-    public Amount getAmount() {
-        return amount;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    /**
-     * Adds a {@code newDebt} to the {@code currentDebts} list of debts and returns a the new list of debts.
-     */
-    public static List<Debt> addDebt(Debt newDebt, List<Debt> currentDebts) {
-        final List<Debt> newDebts = new ArrayList<>();
-        for (Debt debt : currentDebts) {
-            newDebts.add(debt);
-        }
-        newDebts.add(newDebt);
-        return newDebts;
-    }
-
-    public static Amount getTotalDebt(TransactionList<Debt> debts) {
-        double totalDebt = 0;
-        for (Debt debt : debts) {
-            totalDebt += debt.getAmount().amount;
-        }
-        return new Amount(totalDebt);
     }
 
     @Override
