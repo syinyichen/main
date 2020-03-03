9a7275cf452681afe637d40bb71380c45a91e1ff
syinyichen <e0313651@u.nus.edu>
2020-03-03 13:15:02 +0800

    Add Loan class and test

---
+++ src/main/java/seedu/address/model/transaction/Loan.java
@@ -0,0 +1,55 @@
+package seedu.address.model.transaction;
+
+import seedu.address.model.tag.Tag;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * Represents a loan, money which the user lends.
+ */
+public class Loan extends Transaction {
+    /**
+     * Constructs a Loans object.
+     */
+    public Loan(Description description, Amount amount, LocalDate date) {
+        super(description, amount, date, new Tag("Loan"));
+    }
+
+    /**
+     * Checks if both Loans are the same.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Loan)) {
+            return false;
+        }
+
+        Loan otherLoan = (Loan) other;
+        return otherLoan.getDescription().equals(getDescription())
+                && otherLoan.getAmount().equals(getAmount())
+                && otherLoan.getDate().equals(getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        // use "l" to differentiate from other transactions
+        return Objects.hash("l", description, amount, date, tag);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(" Loan description: ")
+                .append(getDescription())
+                .append(" Amount: ")
+                .append(getAmount())
+                .append(" Date: ")
+                .append(getDate());
+        return builder.toString();
+    }
+}
