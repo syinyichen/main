777290d5002f76cd8559327180acdb4aa2936c2e
syinyichen <e0313651@u.nus.edu>
2020-03-07 16:41:08 +0800

    Add tests

---
+++ src/test/java/seedu/address/logic/commands/people/PeopleRemindAllCommandTest.java
@@ -0,0 +1,94 @@
+package seedu.address.logic.commands.people;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BOB;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalUser.getTypicalUserData;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.AddressBookBuilder;
+import seedu.address.testutil.PersonBuilder;
+
+public class PeopleRemindAllCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_emptyUserData_success() {
+
+        PeopleRemindallCommand peopleRemindallCommand = new PeopleRemindallCommand();
+
+        assertCommandFailure(peopleRemindallCommand, model, Messages.MESSAGE_EMPTY_USER_DATA);
+    }
+
+    @Test
+    public void execute_unfilteredList_success() {
+        model.setUserData(getTypicalUserData());
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        String expectedMessage = "";
+        for (Person p: lastShownList) {
+            if (!p.getLoans().getTotal().isZero()) {
+                expectedMessage += "Reminded " + p.getName().toString() + " to return "
+                        + p.getLoans().getTotal().toString() + "!\n";
+            }
+        }
+        expectedMessage += "Sharkie has sent copies of the reminders to your email!";
+
+        assertCommandSuccess(new PeopleRemindallCommand(), model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        model.setUserData(getTypicalUserData());
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        String expectedMessage = "";
+        for (Person p: lastShownList) {
+            if (!p.getLoans().getTotal().isZero()) {
+                expectedMessage += "Reminded " + p.getName().toString() + " to return "
+                        + p.getLoans().getTotal().toString() + "!\n";
+            }
+        }
+        expectedMessage += "Sharkie has sent copies of the reminders to your email!";
+
+        assertCommandSuccess(new PeopleRemindallCommand(), model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_allPeopleWithZeroDebt_failure() {
+        model.setUserData(getTypicalUserData());
+        Person alice = new PersonBuilder(ALICE).withLoans().build();
+        Person bob = new PersonBuilder(BOB).withLoans().build();
+        Person carl = new PersonBuilder(CARL).withLoans().build();
+        AddressBook ab = new AddressBookBuilder()
+                .withPerson(alice)
+                .withPerson(bob)
+                .withPerson(carl).build();
+        model.setAddressBook(ab);
+
+        String expectedMessage = "No one owes you money :(";
+
+        assertCommandFailure(new PeopleRemindallCommand(), model, expectedMessage);
+    }
+}
--- src/test/java/seedu/address/logic/commands/people/PeopleRemindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/people/PeopleRemindCommandTest.java
@@ -3,16 +3,19 @@ package seedu.address.logic.commands.people;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalUser.getTypicalUserData;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -27,6 +30,42 @@ public class PeopleRemindCommandTest {
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_emptyUserData_success() {
+
+        PeopleRemindCommand peopleRemindCommand = new PeopleRemindCommand(INDEX_SECOND_PERSON);
+
+        assertCommandFailure(peopleRemindCommand, model, Messages.MESSAGE_EMPTY_USER_DATA);
+    }
+
+    @Test
+    public void execute_unfilteredList_success() {
+        model.setUserData(getTypicalUserData());
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        Person personUserReminds = lastShownList.get(INDEX_SECOND_PERSON.getZeroBased());
+
+        PeopleRemindCommand peopleRemindCommand = new PeopleRemindCommand(INDEX_SECOND_PERSON);
+
+        String expectedMessage = String.format(PeopleRemindCommand.MESSAGE_REMIND_SUCCESS,
+                personUserReminds.getName(), personUserReminds.getLoans().getTotal());
+
+        assertCommandSuccess(peopleRemindCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        model.setUserData(getTypicalUserData());
+        Person personUserReminds = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+
+        PeopleRemindCommand peopleRemindCommand = new PeopleRemindCommand(INDEX_SECOND_PERSON);
+
+        String expectedMessage = String.format(PeopleRemindCommand.MESSAGE_REMIND_SUCCESS,
+                personUserReminds.getName(), personUserReminds.getLoans().getTotal());
+
+        assertCommandSuccess(peopleRemindCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
