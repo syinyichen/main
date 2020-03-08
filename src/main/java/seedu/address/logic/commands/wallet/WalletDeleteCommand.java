239ca2bceb9a9dbe32d7f25cf5fdff5e7b79d09d
gracegrape <e0324121@u.nus.edu>
2020-03-08 18:36:13 +0800

    Added changes to Wallet package

--- src/main/java/seedu/address/logic/commands/wallet/WalletDeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/wallet/WalletDeleteCommand.java
@@ -1,5 +1,53 @@
 package seedu.address.logic.commands.wallet;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliPrefix.PEOPLE_COMMAND_TYPE;
+import static seedu.address.logic.parser.CliPrefix.WALLET_COMMAND_TYPE;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.people.PeopleDeleteCommand;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
 public class WalletDeleteCommand {
-    
+    public static final String COMMAND_WORD = "delete";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the expense/income identified by the index number used in the displayed transaction list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + WALLET_COMMAND_TYPE + " "
+            + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_TRANSACTION_SUCCESS = "Deleted Income/Expense: %1$s";
+
+    private final Index targetIndex;
+
+    public WalletDeleteCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteTransaction(personToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_TRANSACTION_SUCCESS, personToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PeopleDeleteCommand // instanceof handles nulls
+                && targetIndex.equals(((PeopleDeleteCommand) other).targetIndex)); // state check
+    }
 }
