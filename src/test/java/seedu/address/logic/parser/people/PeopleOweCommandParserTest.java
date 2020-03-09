333cb64b75010df851570b276ce4a0fee870bef1
CheyanneSim <Cheyanne@u.nus.edu>
2020-03-09 17:34:48 +0800

    Edit tests for invalid preamble

--- src/test/java/seedu/address/logic/parser/people/PeopleOweCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/people/PeopleOweCommandParserTest.java
@@ -66,16 +66,20 @@ public class PeopleOweCommandParserTest {
     @Test
     public void parse_invalidPreamble_failure() {
         // negative index
-        assertParseFailure(parser, "-5" + NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "-5" + VALID_NAME_AMY + " " + PREFIX_NAME + VALID_DESC_AMY + " "
+                + PREFIX_AMOUNT + VALID_AMOUNT_AMY, MESSAGE_INVALID_FORMAT);
 
         // zero index
-        assertParseFailure(parser, "0" + NAME_DESC_AMY, MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "0" + VALID_NAME_AMY + " " + PREFIX_NAME + VALID_DESC_AMY + " "
+                + PREFIX_AMOUNT + VALID_AMOUNT_AMY, MESSAGE_INVALID_FORMAT);
 
         // invalid arguments being parsed as preamble
-        assertParseFailure(parser, "1 some random string", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1 some random string" + VALID_NAME_AMY + " " + PREFIX_NAME
+                + VALID_DESC_AMY + " " + PREFIX_AMOUNT + VALID_AMOUNT_AMY, MESSAGE_INVALID_FORMAT);
 
         // invalid prefix being parsed as preamble
-        assertParseFailure(parser, "1 i/ string", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1 i/ string" + VALID_NAME_AMY + " " + PREFIX_NAME + VALID_DESC_AMY + " "
+                + PREFIX_AMOUNT + VALID_AMOUNT_AMY, MESSAGE_INVALID_FORMAT);
     }
 
     @Test
---
+++ src/test/java/seedu/address/logic/parser/people/PeopleReturnedCommandParserTest.java
@@ -0,0 +1,4 @@
+package seedu.address.logic.parser.people;
+
+public class PeopleReturnedCommandParserTest {
+}
