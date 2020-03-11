4be404a970a26bd708292fbd4c1b9bdb122ab9a6
Daniel Hoe <danielhoe97@gmail.com>
2020-03-11 23:03:18 +0800

    Added tests

--- src/test/java/seedu/address/model/transaction/BudgetListTest.java
+++ src/test/java/seedu/address/model/transaction/BudgetListTest.java
@@ -2,6 +2,7 @@ package seedu.address.model.transaction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalWallet.BUDGET_APRIL_2020;
@@ -15,6 +16,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.transaction.exceptions.BudgetNotFoundException;
 import seedu.address.testutil.BudgetBuilder;
 
 public class BudgetListTest {
@@ -67,12 +69,12 @@ public class BudgetListTest {
     }
 
     @Test
-    public void hasBudget_nullBudget_throwsNullPointerException() {
+    public void containsBudget_nullBudget_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> budgetList.contains(null));
     }
 
     @Test
-    public void hasBudget_existingBudget_returnsTrue() {
+    public void containsBudget_existingBudget_returnsTrue() {
         BudgetList<Budget> tempBudgetList = new BudgetList<>();
         tempBudgetList.add(BUDGET_JAN_2010);
         tempBudgetList.add(BUDGET_APRIL_2020);
@@ -82,7 +84,7 @@ public class BudgetListTest {
     }
 
     @Test
-    public void hasBudget_nonExistentBudget_returnsFalse() {
+    public void containsBudget_nonExistentBudget_returnsFalse() {
         BudgetList<Budget> tempBudgetList = new BudgetList<>();
         Budget nonExistentBudget =
                 new BudgetBuilder().withAmount("3000").withMonth("09").withYear("2069").buildBudget();
@@ -101,11 +103,78 @@ public class BudgetListTest {
     }
 
     @Test
+    public void setBudget_budgetNotInList_throwsBudgetNotFoundException() {
+        BudgetList<Budget> tempBudgetList = new BudgetList<>();
+        assertThrows(BudgetNotFoundException.class, () -> tempBudgetList.setBudget(BUDGET_JAN_2010, BUDGET_APRIL_2020));
+    }
+
+    @Test
+    public void setBudget_budgetInList_success() {
+        BudgetList<Budget> expectedBudgetList = new BudgetList<>();
+        expectedBudgetList.add(BUDGET_APRIL_2020);
+
+        BudgetList<Budget> tempBudgetList = new BudgetList<>();
+        tempBudgetList.add(BUDGET_JAN_2010);
+        tempBudgetList.setBudget(BUDGET_JAN_2010, BUDGET_APRIL_2020);
+        assertEquals(expectedBudgetList, tempBudgetList);
+    }
+
+    @Test
     public void addBudget_normalBudget_budgetAddedToList() {
         List<Budget> expectedBudgetList = List.of(BUDGET_JAN_2010);
         BudgetList<Budget> tempBudgetList = new BudgetList<>();
         tempBudgetList.add(BUDGET_JAN_2010);
 
         assertEquals(expectedBudgetList, tempBudgetList.internalList);
+    }
+
+    @Test
+    public void containsBudgetOf_nullArguments_throwsNullPointerExcetption() {
+        Budget tempBudget = Budget.getDefault();
+
+        assertThrows(NullPointerException.class, () -> budgetList.containsBudgetOf(null, null));
+        assertThrows(NullPointerException.class, () -> budgetList.containsBudgetOf(tempBudget.getMonth(), null));
+        assertThrows(NullPointerException.class, () -> budgetList.containsBudgetOf(null, tempBudget.getYear()));
+    }
+
+    @Test
+    public void containsBudgetOf_budgetNotInList_returnsFalse() {
+        BudgetList<Budget> tempBudgetList = new BudgetList<>();
+        tempBudgetList.add(BUDGET_APRIL_2020);
+
+        assertFalse(tempBudgetList.containsBudgetOf(BUDGET_JAN_2010.getMonth(), BUDGET_JAN_2010.getYear()));
+    }
+
+    @Test
+    public void containsBudgetOf_budgetInList_returnsTrue() {
+        BudgetList<Budget> tempBudgetList = new BudgetList<>();
+        tempBudgetList.add(BUDGET_JAN_2010);
+        tempBudgetList.add(BUDGET_APRIL_2020);
+
+        assertTrue(tempBudgetList.containsBudgetOf(BUDGET_JAN_2010.getMonth(), BUDGET_JAN_2010.getYear()));
+        assertTrue(tempBudgetList.containsBudgetOf(BUDGET_APRIL_2020.getMonth(), BUDGET_APRIL_2020.getYear()));
+    }
+
+    @Test
+    public void equals_sameLists_returnsTrue() {
+        BudgetList<Budget> tempBudgetListA = new BudgetList<>();
+        tempBudgetListA.add(BUDGET_JAN_2010);
+
+        BudgetList<Budget> tempBudgetListB = new BudgetList<>();
+        tempBudgetListB.add(BUDGET_JAN_2010);
+
+        assertEquals(tempBudgetListA, tempBudgetListB);
+    }
+
+    @Test
+    public void equals_differentLists_returnsFalse() {
+        BudgetList<Budget> tempBudgetListA = new BudgetList<>();
+        tempBudgetListA.add(BUDGET_JAN_2010);
+
+        BudgetList<Budget> tempBudgetListB = new BudgetList<>();
+        tempBudgetListA.add(BUDGET_JAN_2010);
+        tempBudgetListB.add(BUDGET_APRIL_2020);
+
+        assertNotEquals(tempBudgetListA, tempBudgetListB);
     }
 }
--- src/test/java/seedu/address/model/transaction/BudgetTest.java
+++ src/test/java/seedu/address/model/transaction/BudgetTest.java
@@ -1,12 +1,54 @@
 package seedu.address.model.transaction;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalWallet.BUDGET_APRIL_2020;
+import static seedu.address.testutil.TypicalWallet.BUDGET_JAN_2010;
+import static seedu.address.testutil.TypicalWallet.VALID_BUDGET_AMOUNT_JAN_2010;
+import static seedu.address.testutil.TypicalWallet.VALID_BUDGET_MONTH_JAN_2010;
+import static seedu.address.testutil.TypicalWallet.VALID_BUDGET_YEAR_JAN_2010;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.BudgetBuilder;
 
 public class BudgetTest {
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new Budget(null, null, null));
+    }
+
+    @Test
+    public void equals_sameObjects_returnsTrue() {
+        Budget budget =
+                new BudgetBuilder().withAmount(VALID_BUDGET_AMOUNT_JAN_2010)
+                        .withMonth(VALID_BUDGET_MONTH_JAN_2010)
+                        .withYear(VALID_BUDGET_YEAR_JAN_2010).buildBudget();
+
+        assertEquals(budget, BUDGET_JAN_2010);
+    }
+
+    @Test
+    public void equals_differentObjects_returnsFalse() {
+        assertNotEquals(BUDGET_JAN_2010, BUDGET_APRIL_2020);
+    }
+
+    @Test
+    public void dateEquals_sameDate_returnsTrue() {
+        Budget budgetA = new BudgetBuilder().withAmount("3000").withMonth("01").withYear("2020").buildBudget();
+        Budget budgetB = new BudgetBuilder().withAmount("6969").withMonth("01").withYear("2020").buildBudget();
+
+        assertTrue(budgetA.dateEquals(budgetB));
+    }
+
+    @Test
+    public void dateEquals_differentDate_returnsFalse() {
+        Budget budgetA = new BudgetBuilder().withAmount("3000").withMonth("02").withYear("2020").buildBudget();
+        Budget budgetB = new BudgetBuilder().withAmount("6969").withMonth("01").withYear("2021").buildBudget();
+
+        assertFalse(budgetA.dateEquals(budgetB));
     }
 }
