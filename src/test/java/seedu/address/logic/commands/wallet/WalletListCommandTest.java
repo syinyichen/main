f76986624d86152e0e31ac48996f19aba91a7a1d
gracegrape <e0324121@u.nus.edu>
2020-03-12 19:13:32 +0800

    Added WalletListCommandTest

--- src/test/java/seedu/address/logic/commands/wallet/WalletListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/wallet/WalletListCommandTest.java
@@ -1,4 +1,23 @@
 package seedu.address.logic.commands.wallet;
 
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalWallet.getTypicalWallet;
+
+import org.junit.jupiter.api.BeforeEach;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
 public class WalletListCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), getTypicalWallet(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), model.getWallet(), new UserPrefs());
+    }
 }
