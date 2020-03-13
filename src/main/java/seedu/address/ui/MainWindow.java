255b6f1a941455733d572bf70803ad95448b1554
gracegrape <e0324121@u.nus.edu>
2020-03-13 20:27:12 +0800

    Added tabs

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -52,9 +52,9 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane statusbarPlaceholder;
-
-    @FXML
-    private StackPane tabDisplayPlaceHolder;
+//
+//    @FXML
+//    private StackPane tabDisplayPlaceHolder;
 
     public MainWindow(Stage primaryStage, Logic logic) {
         super(FXML, primaryStage);
@@ -115,16 +115,11 @@ public class MainWindow extends UiPart<Stage> {
      * Fills up all the placeholders of this window.
      */
     void fillInnerParts() {
-        tabDisplay = new TabDisplay();
-        tabDisplayPlaceHolder.getChildren().add(tabDisplay.getRoot());
-//        tabDisplay
-
-
+//        tabDisplay = new TabDisplay();
+//        tabDisplayPlaceHolder.getChildren().add(tabDisplay.getRoot());
 
         personListPanel = new PersonListPanel(logic.getFilteredPersonList());
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
-
-        //tabDisplayPlaceHolder.getChildren().add(personListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
