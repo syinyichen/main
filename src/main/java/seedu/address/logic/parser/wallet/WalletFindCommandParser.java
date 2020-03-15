8a37fa8ba92afb2a1a9e25c974d429bd01d444fa
gracegrape <e0324121@u.nus.edu>
2020-03-15 10:44:31 +0800

    Added wallet find command parser

--- src/main/java/seedu/address/logic/parser/wallet/WalletFindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/wallet/WalletFindCommandParser.java
@@ -1,4 +1,29 @@
 package seedu.address.logic.parser.wallet;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.wallet.WalletFindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.transaction.DescriptionContainsKeywordsPredicate;
+
 public class WalletFindCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public WalletFindCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, WalletFindCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new WalletFindCommand(new DescriptionContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
 }
