bc41920118622e289cf4aac04b64722c65e16152
gracegrape <e0324121@u.nus.edu>
2020-03-16 13:01:46 +0800

    edited code based on reviews

--- src/test/java/seedu/address/commons/util/StringUtilTest.java
+++ src/test/java/seedu/address/commons/util/StringUtilTest.java
@@ -93,9 +93,9 @@ public class StringUtilTest {
      *   - last word in sentence
      *   - middle word in sentence
      *   - matches multiple words
+     *   - query word matches part of a sentence word
      *
      * Possible scenarios returning false:
-     *   - query word matches part of a sentence word
      *   - sentence word matches part of the query word
      *
      * The test method below tries to verify all above with a reasonably low number of test cases.
@@ -108,8 +108,10 @@ public class StringUtilTest {
         assertFalse(StringUtil.containsWordIgnoreCase("", "abc")); // Boundary case
         assertFalse(StringUtil.containsWordIgnoreCase("    ", "123"));
 
-        // Matches a partial word only
+        // Query word matches part of a sentence word
         assertTrue(StringUtil.containsWordIgnoreCase("aaa bbb ccc", "bb")); // Sentence word bigger than query word
+
+        // Sentence words matches part of query word
         assertFalse(StringUtil.containsWordIgnoreCase("aaa bbb ccc", "bbbb")); // Query word bigger than sentence word
 
         // Matches word in the sentence, different upper/lower case letters
