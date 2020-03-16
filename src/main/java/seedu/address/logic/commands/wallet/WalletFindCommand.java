c7ab769ac965259470fc21cb5d144780f8067401
gracegrape <e0324121@u.nus.edu>
2020-03-16 19:52:28 +0800

    fixed review stuff

--- src/main/java/seedu/address/logic/commands/wallet/WalletFindCommand.java
+++ src/main/java/seedu/address/logic/commands/wallet/WalletFindCommand.java
@@ -15,8 +15,8 @@ import seedu.address.model.transaction.DescriptionContainsKeywordsPredicate;
 public class WalletFindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all transactions whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all transactions with description containing "
+            + "any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + WALLET_COMMAND_TYPE + " "
             + COMMAND_WORD + " rice water chicken";
