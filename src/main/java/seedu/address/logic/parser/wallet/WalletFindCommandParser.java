29c725c191ee70cbfb37ae7fdcebc746babd811b
gracegrape <e0324121@u.nus.edu>
2020-03-19 22:13:01 +0800

    Editing parser

--- src/main/java/seedu/address/logic/parser/wallet/WalletFindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/wallet/WalletFindCommandParser.java
@@ -1,12 +1,23 @@
 package seedu.address.logic.parser.wallet;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AMOUNT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
 
 import seedu.address.logic.commands.wallet.WalletFindCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.transaction.Amount;
+import seedu.address.model.transaction.Date;
+import seedu.address.model.transaction.Description;
 import seedu.address.model.transaction.DescriptionContainsKeywordsPredicate;
 
 /**
@@ -20,6 +31,7 @@ public class WalletFindCommandParser implements Parser<WalletFindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public WalletFindCommand parse(String args) throws ParseException {
+        /*
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
@@ -29,5 +41,36 @@ public class WalletFindCommandParser implements Parser<WalletFindCommand> {
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
         return new WalletFindCommand(new DescriptionContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+         */
+
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_AMOUNT, PREFIX_DATE,
+                PREFIX_TAG);
+
+        if (!argMultimap.getValue(PREFIX_NAME).isPresent() && !argMultimap.getValue(PREFIX_AMOUNT).isPresent()
+                && !argMultimap.getValue(PREFIX_DATE).isPresent() && !argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, WalletFindCommand.MESSAGE_USAGE));
+        }
+
+        Description description;
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            description = ParserUtil.parseDescription(argMultimap.getValue(PREFIX_NAME).get());
+        }
+
+        Amount amount;
+        if (argMultimap.getValue(PREFIX_AMOUNT).isPresent()) {
+            amount = ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get());
+        }
+
+        Date date;
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get());
+        }
+
+        Tag tag;
+        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            tag = ParserUtil.parseTag(argMultimap.getValue(PREFIX_TAG).get());
+        }
+
+
     }
 }
