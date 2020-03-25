92b12f01b177fe01450e8db4beeefc6f814e7e84
gracegrape <e0324121@u.nus.edu>
2020-03-25 22:27:53 +0800

    Done

--- src/main/java/seedu/address/model/transaction/DateContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/transaction/DateContainsKeywordsPredicate.java
@@ -3,15 +3,14 @@ package seedu.address.model.transaction;
 import java.util.List;
 
 public class DateContainsKeywordsPredicate extends WalletPredicate {
-    public AmountContainsKeywordsPredicate(List<String> keywords) {
+    public DateContainsKeywordsPredicate(List<String> keywords) {
         super(keywords);
     }
 
     @Override
     public boolean test(Transaction transaction) {
         return keywords.stream()
-                .anyMatch(keyword -> (int) Double.parseDouble(keyword) ==
-                        (int) transaction.getAmount().amount);
+                .anyMatch(keyword -> keyword.equals(transaction.getDate().toString()));
     }
 
     @Override
--- src/main/java/seedu/address/model/transaction/TagContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/transaction/TagContainsKeywordsPredicate.java
@@ -1,4 +1,26 @@
 package seedu.address.model.transaction;
 
-public class TagContainsKeywordsPredicate {
+import java.util.List;
+
+import seedu.address.commons.util.StringUtil;
+
+public class TagContainsKeywordsPredicate  extends WalletPredicate{
+    public TagContainsKeywordsPredicate(List<String> keywords) {
+        super(keywords);
+    }
+
+    @Override
+    public boolean test(Transaction transaction) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(
+                        transaction.getTag().tagName, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AmountContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((AmountContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
 }
