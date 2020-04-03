788c0a0b0d0e5ebbec359d288ac98447c914c28a
Daniel Hoe <danielhoe97@gmail.com>
2020-04-03 20:17:57 +0800

    Fixed budget command taking in only month or year

--- src/main/java/seedu/address/logic/parser/wallet/WalletBudgetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/wallet/WalletBudgetCommandParser.java
@@ -34,7 +34,8 @@ public class WalletBudgetCommandParser implements Parser<WalletBudgetCommand> {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_AMOUNT, PREFIX_MONTH, PREFIX_YEAR);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_AMOUNT)
-                || !argMultimap.getPreamble().isEmpty()) {
+                || !argMultimap.getPreamble().isEmpty()
+                || !arePrefixesPresent(argMultimap, PREFIX_MONTH, PREFIX_YEAR)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, WalletBudgetCommand.MESSAGE_USAGE));
         }
 
